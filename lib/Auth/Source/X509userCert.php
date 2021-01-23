9136e73082737cd4e17725a2de53234bc582d07f
Tim van Dijen <tvdijen@gmail.com>
2021-01-23 17:48:27 +0100

    Use strict_types=1

--- lib/Auth/Source/X509userCert.php
+++ lib/Auth/Source/X509userCert.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SimpleSAML\Module\authX509\Auth\Source;
 
 use Exception;
--- tests/lib/Controller/ExpiryWarningTest.php
+++ tests/lib/Controller/ExpiryWarningTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SimpleSAML\Test\Module\authX509\Controller;
 
 use PHPUnit\Framework\TestCase;
